ace8e3205b7fa31db93f572750271c268bad777f
Amol Khade <khade4982@gmail.com>
2024-07-26 11:01:08 +0530

    kyc api added

--- src/server.js
+++ src/server.js
@@ -35,38 +35,7 @@ app.get("/dashboardData", (req, res) => {
   }
 });
 
-const msg =
-  "API Running, Please Use Valid API Endpoint as per API Documentation and Request Method (GET, POST, PUT, DELETE, PATCH) to get valid response.";
 
-app.get("/*", (req, res) => {
-  res.send(msg);
-  res.status(200);
-});
-
-app.post("/*", (req, res) => {
-  res.send(msg);
-  res.status(200);
-});
-
-app.put("/*", (req, res) => {
-  res.send(msg);
-  res.status(200);
-});
-
-app.patch("/*", (req, res) => {
-  res.send(msg);
-  res.status(200);
-});
-
-app.delete("/*", (req, res) => {
-  res.send(msg);
-  res.status(200);
-});
-
-app.head("/*", (req, res) => {
-  res.send(msg);
-  res.status(200);
-});
 
 // Auth Routes
 app.use("/api/auth", require("./Backend/auth"));
